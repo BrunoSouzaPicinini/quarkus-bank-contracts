9e9c704432d7a4e89803c58e94ef8a672ef07e0e
Bruno de Souza Picinini <brunosouzapicinini@gmail.com>
2025-01-19 23:26:01 -0300

    fix typo

--- src/main/java/com/bspicinini/config/MapStructConfig.java
+++ src/main/java/com/bspicinini/config/MapStructConfig.java
@@ -4,7 +4,7 @@ import org.mapstruct.MapperConfig;
 import org.mapstruct.ReportingPolicy;
 
 @MapperConfig(
-    componentModel = "cdi",
+    componentModel = "jakarta",
     unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
 public interface MapStructConfig {
